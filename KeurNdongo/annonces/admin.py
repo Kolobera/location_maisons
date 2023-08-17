094851f13c24269c88ac3a93573b288712147774
Ousmane Konate <konatealim@gmail.com>
2023-08-17 09:28:39 +0200

    mise en place acceuil

--- KeurNdongo/annonces/admin.py
+++ KeurNdongo/annonces/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from annonces.models import Annonce
+admin.site.register(Annonce)
--- KeurNdongo/annonces/urls.py
+++ KeurNdongo/annonces/urls.py
@@ -1,9 +1,12 @@
 # annonces/urls.py
+from django.conf import settings
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('annonces/ajouter/', views.ajouter_annonce, name='ajouter'),
     path('', views.accueil, name="home"),
     # Autres URLs pour les annonces
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
