3e07bc6a358d4f0202caa7149ce9687d468de31c
User <user@SAboubacars-MacBook.local>
2023-08-17 00:15:43 +0000

    creation models

--- KeurNdongo/annonces/models.py
+++ KeurNdongo/annonces/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from user.models import User
+
+class Annonce(models.Model):
+    TANDING_CHOICES = [
+        ('standard', 'Standard'),
+        ('moyen', 'Moyen'),
+        ('haut', 'Haut'),
+    ]
+
+    titre = models.CharField(max_length=200)
+    proprietaire = models.ForeignKey(User, on_delete=models.CASCADE, related_name='annonces_proprietaire')
+    locataire = models.ForeignKey(User, on_delete=models.CASCADE, related_name='annonces_locataire')
+    ville = models.CharField(max_length=100)
+    quartier = models.CharField(max_length=100)
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    standing = models.CharField(max_length=10, choices=TANDING_CHOICES)
+    date_creation = models.DateTimeField(auto_now_add=True)
+
--- KeurNdongo/users/models.py
+++ KeurNdongo/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    tel = models.CharField(max_length=15)
+    is_student = models.BooleanField(default=True)
+
+class Favoris(models.Model):
+    annonce = models.ForeignKey('annonces.Annonce', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
